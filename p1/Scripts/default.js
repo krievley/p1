e32a6a479512986587f077931be74814eaef5386
Kristin Rievley <krievley@gmail.com>
2014-09-15 15:35:28 -0400

    Added Footer to P1

---
+++ p1/Scripts/default.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('.ins-col').mouseenter(function () {
+        $(this).find('.icon').animate({
+            height: '100px',
+            width: '100px'
+        }, "fast");
+        $(this).find('.link').css("font-size", "20px");
+    });
+    $('.ins-col').mouseleave(function () {
+        $(this).find('.icon').animate({
+            height: '75px',
+            width: '75px'
+        }, "fast");
+        $(this).find('.link').css("font-size", "16px");
+    });
+});
